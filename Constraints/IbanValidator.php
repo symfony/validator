14e38880eb644a8a8bc63df490abafce356ef405
Bernhard Schussek <bschussek@gmail.com>
2014-04-10 17:39:39 +0200

    [Validator] Fixed value-to-string conversion in constraint violations
    Simplified the IBAN validation algorithm and added more comments.

--- Constraints/IbanValidator.php
+++ Constraints/IbanValidator.php
@@ -13,10 +13,12 @@ namespace Symfony\Component\Validator\Constraints;
 
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 
 /**
  * @author Manuel Reinhard <manu@sprain.ch>
  * @author Michael Schummel
+ * @author Bernhard Schussek <bschussek@gmail.com>
  * @link http://www.michael-schummel.de/2007/10/05/iban-prufung-mit-php/
  */
 class IbanValidator extends ConstraintValidator
@@ -30,41 +32,98 @@ class IbanValidator extends ConstraintValidator
             return;
         }
 
-        // An IBAN without a country code is not an IBAN.
-        if (0 === preg_match('/[A-Z]/', $value)) {
-            $this->context->addViolation($constraint->message, array('{{ value }}' => $value));
+        if (!is_scalar($value) && !(is_object($value) && method_exists($value, '__toString'))) {
+            throw new UnexpectedTypeException($value, 'string');
+        }
+
+        // Remove spaces
+        $canonicalized = str_replace(' ', '', $value);
+
+        if (strlen($canonicalized) < 4) {
+            $this->context->addViolation($constraint->message, array(
+                '{{ value }}' => $value,
+            ));
 
             return;
         }
 
-        $teststring = preg_replace('/\s+/', '', $value);
-
-        if (strlen($teststring) < 4) {
-            $this->context->addViolation($constraint->message, array('{{ value }}' => $value));
+        // The IBAN must have at least 4 characters, start with a country
+        // code and contain only digits and (uppercase) characters
+        if (strlen($canonicalized) < 4 || !ctype_upper($canonicalized{0})
+            || !ctype_upper($canonicalized{1}) || !ctype_alnum($canonicalized)) {
+            $this->context->addViolation($constraint->message, array(
+                '{{ value }}' => $value,
+            ));
 
             return;
         }
 
-        $teststring = substr($teststring, 4)
-            .strval(ord($teststring{0}) - 55)
-            .strval(ord($teststring{1}) - 55)
-            .substr($teststring, 2, 2);
+        // Move the first four characters to the end
+        // e.g. CH93 0076 2011 6238 5295 7
+        //   -> 0076 2011 6238 5295 7 CH93
+        $canonicalized = substr($canonicalized, 4).substr($canonicalized, 0, 4);
 
-        $teststring = preg_replace_callback('/[A-Z]/', function ($letter) {
-            return intval(ord(strtolower($letter[0])) - 87);
-        }, $teststring);
+        // Convert all remaining letters to their ordinals
+        // The result is an integer, which is too large for PHP's int
+        // data type, so we store it in a string instead.
+        // e.g. 0076 2011 6238 5295 7 CH93
+        //   -> 0076 2011 6238 5295 7 121893
+        $bigInt = $this->toBigInt($canonicalized);
 
+        if (false === $bigInt) {
+            $this->context->addViolation($constraint->message, array(
+                '{{ value }}' => $value,
+            ));
+
+            return;
+        }
+
+        // Do a modulo-97 operation on the large integer
+        // We cannot use PHP's modulo operator, so we calculate the
+        // modulo step-wisely instead
+        if (1 !== $this->bigModulo97($bigInt)) {
+            $this->context->addViolation($constraint->message, array(
+                '{{ value }}' => $value,
+            ));
+
+            return;
+        }
+    }
+
+    private function toBigInt($string)
+    {
+        $length = strlen($string);
+        $bigInt = '';
+
+        for ($i = 0; $i < $length; ++$i) {
+            // Convert uppercase characters to ordinals, starting with 10 for "A"
+            if (ctype_upper($string{$i})) {
+                $bigInt .= (ord($string{$i}) - 55);
+
+                continue;
+            }
+
+            // Disallow lowercase characters
+            if (ctype_lower($string{$i})) {
+                return false;
+            }
+
+            // Simply append digits
+            $bigInt .= $string{$i};
+        }
+
+        return $bigInt;
+    }
+
+    private function bigModulo97($bigInt)
+    {
+        $parts = str_split($bigInt, 7);
         $rest = 0;
-        $strlen = strlen($teststring);
-        for ($pos = 0; $pos < $strlen; $pos += 7) {
-            $part = strval($rest).substr($teststring, $pos, 7);
-            $rest = intval($part) % 97;
+
+        foreach ($parts as $part) {
+            $rest = ($rest.$part) % 97;
         }
 
-        if ($rest != 1) {
-            $this->context->addViolation($constraint->message, array('{{ value }}' => $value));
-
-            return;
-        }
+        return $rest;
     }
 }
