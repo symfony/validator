symfony/symfony

[Validator] Added context object method callback to choice validator

src/Symfony/Component/Validator/Constraints/ChoiceValidator.php
@@ -48,6 +48,7 @@ class ChoiceValidator extends ConstraintValidator
 
         if ($constraint->callback) {
             if (!is_callable($choices = array($this->context->getClassName(), $constraint->callback))
+                && !is_callable($choices = array($this->context->getObject(), $constraint->callback))
                 && !is_callable($choices = $constraint->callback)
             ) {
                 throw new ConstraintDefinitionException('The Choice constraint expects a valid callback');

symfony/validator

Constraints/ChoiceValidator.php
@@ -47,7 +47,8 @@ class ChoiceValidator extends ConstraintValidator
         }
 
         if ($constraint->callback) {
-            if (!is_callable($choices = array($this->context->getClassName(), $constraint->callback))
+            if (!is_callable($choices = array($this->context->getObject(), $constraint->callback))
+                && !is_callable($choices = array($this->context->getClassName(), $constraint->callback))
                 && !is_callable($choices = $constraint->callback)
             ) {
                 throw new ConstraintDefinitionException('The Choice constraint expects a valid callback');