198faf4be227c436df366601b1a0109ef59e6e92
Nicolas Grekas <nicolas.grekas@gmail.com>
2025-09-04 11:40:24 +0200

    [DependencyInjection][Validator] Remove needless code

--- Constraint.php
+++ Constraint.php
@@ -27,7 +27,6 @@ use Symfony\Component\Validator\Exception\MissingOptionsException;
  *
  * @author Bernhard Schussek <bschussek@gmail.com>
  */
-#[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
 abstract class Constraint
 {
     /**
