3ca4a1b054c589470871d90fef40f90fdbb3a269
Vincent QUATREVIEUX <vquatrevieux@b2pweb.com>
2025-10-01 10:36:54 +0200

    [Validator] bad value on AbstractComparison constructor using array parameter

--- Constraints/AbstractComparison.php
+++ Constraints/AbstractComparison.php
@@ -36,6 +36,7 @@ abstract class AbstractComparison extends Constraint
             trigger_deprecation('symfony/validator', '7.3', 'Passing an array of options to configure the "%s" constraint is deprecated, use named arguments instead.', static::class);
 
             $options = array_merge($value, $options ?? []);
+            $value = null;
         } elseif (null !== $value) {
             if (\is_array($options)) {
                 trigger_deprecation('symfony/validator', '7.3', 'Passing an array of options to configure the "%s" constraint is deprecated, use named arguments instead.', static::class);
