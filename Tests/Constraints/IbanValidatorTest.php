df99ff55ee435de3ac8e10c361567379c947c488
Lazizbek Ergashev <lazerg2@gmail.com>
2026-09-07 21:18:15 +0500

    [Validator] Drop unreachable territory keys from IBAN formats

--- Tests/Constraints/IbanValidatorTest.php
+++ Tests/Constraints/IbanValidatorTest.php
@@ -443,6 +443,22 @@ class IbanValidatorTest extends ConstraintValidatorTestCase
             ['AQ'],
             ['AS'],
             ['AW'],
+            ['AX'],
+            ['BL'],
+            ['GF'],
+            ['GG'],
+            ['GP'],
+            ['IM'],
+            ['JE'],
+            ['MF'],
+            ['MQ'],
+            ['NC'],
+            ['PF'],
+            ['PM'],
+            ['RE'],
+            ['TF'],
+            ['WF'],
+            ['YT'],
         ];
     }
 
